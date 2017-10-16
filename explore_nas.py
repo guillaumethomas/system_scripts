46cc27356c567c7ec281781ac4e5d3f8669c3c09
Guillaume THOMAS <guillaumethomas@outlook.com>
2017-10-15 23:20:39 -0700

    more progress

--- explore_nas.py
+++ explore_nas.py
@@ -7,22 +7,29 @@ def read_nas_fs(location):
     return a fast list of file
     '''
     movie_ext_set = {'.M4V', '.avi', '.m4v', '.mkv', '.mp4'}
-
-    tmp_list = list(map(lambda x: x[2], walk(location)))
-    list_file = [j for i in tmp_list for j in i if i != []]
-    # extension = set([i[-4:] for i in list_file])
-    list_file = [i for i in list_file if i[-4:] in movie_ext_set]
-    return list_file
+    all_data = list(walk(location))
+    tmp_list = list(map(lambda x: x[2], all_data))
+    list_file = [j for i in tmp_list for j in i
+                 if i != [] and j[-4:] in movie_ext_set]
+    return all_data, list_file
 
 
 if __name__ == '__main__':
     '''
 
     '''
-    locations = ['/Volumes/Public/Shared Videos', '/Volumes/service/DLNA']
-
-    for location in locations:
-        list_films = read_nas_fs(location)
-        # print(list_films)
-        print(len(list_films))
-        # print(extension)
+    locations = {'WS': '/Volumes/Public/Shared Videos',
+                 'LG': '/Volumes/service/DLNA'}
+    all_data = list()
+    list_films = list()
+    nas = dict()
+    for location in locations.items():
+        nas[location[0]] = read_nas_fs(location[1])
+        # print(len(list_films[-1]))
+    print('{}\n'.format(nas.keys()))
+    compare = [i[1] for i in nas.values()]
+    for i in compare:
+        i.sort()
+    not_in = [j for j in compare[1] if j not in compare[0]]
+    for i in not_in:
+        print(i)
